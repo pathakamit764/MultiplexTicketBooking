7c03f46695f5fb15669198bada0a7249b8984e8e
Amit Pathak <pathakamit764@gmail.com>
2022-04-13 17:14:34 +0530

    Commit

---
+++ src/main/java/Theatre/MutiplexTicketBooking/MutiplexTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package Theatre.MutiplexTicketBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutiplexTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutiplexTicketBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Theatre/MutiplexTicketBooking/MutiplexTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package Theatre.MutiplexTicketBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutiplexTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
